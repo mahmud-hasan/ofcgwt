e85f10255f8142316110b10dcf636d82f496caf2
gslender <gslender@af36f908-a054-0410-94f4-133e705c06f9>
2008-08-17 07:54:49 +0000

    add compiled demo

--- src/com/gwttest/client/test.java
+++ src/com/gwttest/client/test.java
@@ -34,6 +34,7 @@ public class test implements EntryPoint {
 		dropBox.addItem("Pie");
 		dropBox.addItem("Area");
 		dropBox.addItem("Bar");
+		dropBox.addItem("Glass Bar");
 	    hp.add(dropBox);
 	    
 	    vp.add(hp);
@@ -63,7 +64,9 @@ public class test implements EntryPoint {
 	        	case 2: // bar
 					chart.loadJSON(getBarChart().toString());
 					break;
-	        		
+	        	case 3: // glass bar
+					chart.loadJSON(getGlassBarChart().toString());
+					break;
 	        	}
 	        }
 	      });
@@ -144,4 +147,94 @@ public class test implements EntryPoint {
 		  .setSize(16);
 		return c;
 	}
+
+	private Chart getGlassBarChart() {
+		Chart c = new Chart("Many data lines","font-size: 20px; color:#0000ff; font-family: Verdana; text-align: center;")
+		  .setYLegend(new Text("Open Flash Chart","color: #736AFF; font-size: 12px;"))
+		  .addElements(new BarChart(com.rednels.ofcgwt.client.model.elements.BarChart.Style.GLASS)
+		  	.setColour("#9933CC")
+		    .addValues(9,6,7,9,5)
+		    .addBars(new BarChart.Bar(7,"#FF0000").setTooltip("RED<br>Mooo<br>#val#"))
+		    .addValues(6,9,7)
+		    .setTooltip("Tip for purple bars<br>val=#val#, top=#top#")
+		  	.setAlpha(0.5f))		
+		  	
+		  	;
+		
+//		    .setXAxis(new XAxis()
+//		      .setTickHeight(5)
+//		      .addLabels("one", "two", "three", "four", "five")
+//		      .addLabels(
+//		        new Label("six")
+//		          .setColour("#0000FF")
+//		          .setSize(30)
+//		          .setRotation(Rotation.VERTICAL),
+//		        new Label("seven")
+//		          .setColour("#0000FF")
+//		          .setSize(30)
+//		          .setRotation(Rotation.VERTICAL),
+//		        new Label("eight")
+//		          .setColour("#8C773E")
+//		          .setSize(16)
+//		          .setRotation(Rotation.DIAGONAL)
+//		          .setVisible(true),
+//		        new Label("nine")
+//		          .setColour("#2683CF")
+//		          .setSize(16)
+//		          .setRotation(Rotation.HORIZONTAL)));
+//
+//		c.getXAxis()
+//		  .setStroke(1)
+//		  .setColour("#428C3E")
+//		  .setGridColour("#86BF83")
+//		  .setSteps(1);
+//		     
+//		c.getXAxis().getLabels()
+//		  .setSteps(1)
+//		  .setRotation(Rotation.VERTICAL)
+//		  .setColour("#ff0000")
+//		  .setSize(16);
+		return c;
+	}
+	/*
+	
+
+      "text--":      "Page views",
+      "font-size--": 10,
+
+    {
+      "type":      "bar_glass",
+      "alpha":     0.5,
+      "colour":    "#44FF44",
+      "tip":       "Tip for green bars<br>val=#val#, top=#top#",
+      "text--":      "Page views",
+      "font-size--": 10,
+      "values" :   [6,7,9,5,9,{"top":7,"colour":"#FF0000", "tip": "RED<br>W000t<br>#val#"},6,5,2]
+    }
+  ],
+
+  "x_axis":{
+    "stroke":       1,
+    "tick_height":  10,
+    "colour":      "#d000d0",
+    "grid_colour": "#00ff00",
+    "labels": {
+      "labels": ["January","February","March","April","May","June","July","August","Spetember"]
+    }
+   },
+
+  "y_axis":{
+    "stroke":      4,
+    "tick_length": 3,
+    "colour":      "#d000d0",
+    "grid_colour": "#00ff00",
+    "offset":      0,
+    "max":         20
+  },
+
+  "tooltip":{
+    "text": "Global Tooltip<br>val=#val#, top=#top#"
+  }
+ 
+	  */
 }
