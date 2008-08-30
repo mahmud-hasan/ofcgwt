4295448f9d6f8e435bc1501f1c809b62655e3dfe
gslender <gslender@af36f908-a054-0410-94f4-133e705c06f9>
2008-08-30 02:12:11 +0000

    changed to LGPL lic

--- trunk/src/com/rednels/ofcgwt/client/IChartListener.java
+++ trunk/src/com/rednels/ofcgwt/client/IChartListener.java
@@ -1,19 +1,20 @@
 /*
- *    Copyright 2008 Grant K Slender
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *   
- */
+Copyright (C) 2008 Grant Slender
+
+This file is part of OFCGWT.
+
+OFCGWT is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+OFCGWT is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See <http://www.gnu.org/licenses/lgpl-3.0.txt>.
+*/
 package com.rednels.ofcgwt.client;
 
 public interface IChartListener {
